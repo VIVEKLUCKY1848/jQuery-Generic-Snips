43273389a98079a9eade317a2d383699b26981b1
VS <VIVEKLUCKY1848@users.noreply.github.com>
2016-03-01 12:06:07 +0530

    Update jsjquerysnips.js

--- jsjquerysnips.js
+++ jsjquerysnips.js
@@ -1,4 +1,4 @@
-//No conflict with condition if the jQuery exists in the page.
+//No conflict with condition, if jQuery is loaded in the page.
 if(window.jQuery && typeof jQuery != 'undefined') {
 	jQuery.noConflict();
 	var $jq = jQuery.noConflict();
