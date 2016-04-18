f7afa75a4d8126c8ed97cca056e1761d20462c41
VS <VIVEKLUCKY1848@users.noreply.github.com>
2016-04-18 18:14:49 +0530

    Update jsjquerysnips.js

--- jsjquerysnips.js
+++ jsjquerysnips.js
@@ -11,3 +11,28 @@ if(window.jQuery && typeof jQuery != 'undefined') {
 	var $jq = jQuery.noConflict();
 	var $jk = jQuery.noConflict();
 }
+
+//Open fancybox on button click start
+var $jq1 = jQuery.noConflict();
+$jq1(document).ready(function() {
+	$jq1('.fancybox').fancybox(
+		{
+			hideOnContentClick: true,
+			width: 712,
+			autoDimensions: true,
+			type: 'iframe',
+			showTitle: false,
+			scrolling: 'yes',
+			onComplete: function() {
+				$jq1('#fancybox-frame').load(function() { // wait for frame to load and then gets it's height
+						$jq1('#fancybox-content').height($jq1(this).contents().find('body').height()+30);
+						$.fancybox.resize();
+				});
+			}
+		}
+	);
+	$jq1('.fyb-btn').click(function() {
+		$jq1(this).parent().find('a').trigger('click');
+	});
+});
+//Open fancybox on button click end
